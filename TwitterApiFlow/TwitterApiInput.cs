d52c742f60c7ead910567b91b6de9ba812f11e30
Takeshi KIRIYA <takeshik@users.sf.net>
2009-01-18 11:01:23 +0000

    Imported commit (revision 74) from Subversion repository
    Add TwitterApiFlow project.
    Add stub codes in TwitterApiFlow.

---
+++ TwitterApiFlow/TwitterApiInput.cs
@@ -0,0 +1,97 @@
+﻿// -*- mode: csharp; encoding: utf-8; -*-
+/* MetaTweet
+ *   Hub system for micro-blog communication services
+ * TwitterApiFlow
+ *   MetaTweet Input/Output modules which provides Twitter access with API
+ *   Part of MetaTweet
+ * Copyright © 2008-2009 Takeshi KIRIYA, XSpect Project <takeshik@xspect.org>
+ * All rights reserved.
+ * 
+ * This file is part of TwitterApiFlow.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>,
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using XSpect.MetaTweet.ObjectModel;
+
+namespace XSpect.MetaTweet
+{
+    public class TwitterApiInput
+        : InputFlowModule
+    {
+        // since_id : int
+        [FlowInterface("/statuses/public_timeline")]
+        public IEnumerable<StorageObject> FetchPublicTimeline(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // id : int | string
+        // since : datetime
+        // count : int
+        // page : int
+        [FlowInterface("/statuses/friends_timeline")]
+        public IEnumerable<StorageObject> FetchFriendsTimeline(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // id : int | string
+        // count : int
+        // since : datetime
+        // since_id : int
+        // page : int
+        [FlowInterface("/statuses/user_timeline")]
+        public IEnumerable<StorageObject> FetchUserTimeline(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // id : int (mandatory)
+        [FlowInterface("/statuses/show")]
+        public IEnumerable<StorageObject> FetchStatus(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // status : string (mandatory)
+        // in_reply_to_status_id : int
+        // source : string
+        [FlowInterface("/statuses/update")]
+        public IEnumerable<StorageObject> UpdateStatus(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // page : int
+        // since : datetime
+        // since_id : int
+        [FlowInterface("/statuses/replies")]
+        public IEnumerable<StorageObject> FetchReplies(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        // id : int (mandatory)
+        [FlowInterface("/statuses/destroy")]
+        public IEnumerable<StorageObject> DestroyStatus(IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TwitterApiFlow/TwitterApiOutput.cs
@@ -0,0 +1,63 @@
+﻿// -*- mode: csharp; encoding: utf-8; -*-
+/* MetaTweet
+ *   Hub system for micro-blog communication services
+ * TwitterApiFlow
+ *   MetaTweet Input/Output modules which provides Twitter access with API
+ *   Part of MetaTweet
+ * Copyright © 2008-2009 Takeshi KIRIYA, XSpect Project <takeshik@xspect.org>
+ * All rights reserved.
+ * 
+ * This file is part of TwitterApiFlow.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>,
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Xml;
+using XSpect.MetaTweet.ObjectModel;
+
+namespace XSpect.MetaTweet
+{
+    public class TwitterApiOutput
+        : OutputFlowModule
+    {
+        [FlowInterface("/.xml")]
+        public XmlDocument OutputXml(IEnumerable<StorageObject> source, IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        [FlowInterface("/.rss")]
+        public XmlDocument OutputRss(IEnumerable<StorageObject> source, IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        [FlowInterface("/.atom")]
+        public XmlDocument OutputAtom(IEnumerable<StorageObject> source, IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+        [FlowInterface("/.json")]
+        public String OutputJson(IEnumerable<StorageObject> source, IDictionary<String, String> args)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
