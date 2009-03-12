15ab9165448506dbd86c1e42ce700d94066d9df4
Takeshi KIRIYA <takeshik@users.sf.net>
2009-03-12 09:25:03 +0000

    Imported commit (revision 195) from Subversion repository
    Fix mistakes.

--- XSpectCommonFramework/Lambda.cs
+++ XSpectCommonFramework/Lambda.cs
@@ -31,86 +31,6 @@ namespace XSpect
     public static class Lambda
         : Object
     {
-        public static void Bind<T>(Action<T> action, T value)
-        {
-            action(value);
-        }
-
-        public static void Bind<T1, T2>(Action<T1, T2> action, T1 value1, T2 value2)
-        {
-            action(value1, value2);
-        }
-
-        public static void Bind<T1, T2, T3>(Action<T1, T2, T3> action, T1 value1, T2 value2, T3 value3)
-        {
-            action(value1, value2, value3);
-        }
-
-        public static void Bind<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action, T1 value1, T2 value2, T3 value3, T4 value4)
-        {
-            action(value1, value2, value3, value4);
-        }
-
-        public static void Bind<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
-        {
-            action(value1, value2, value3, value4, value5);
-        }
-
-        public static void Bind<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> action, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6)
-        {
-            action(value1, value2, value3, value4, value5, value6);
-        }
-
-        public static void Bind<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7> action, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7)
-        {
-            action(value1, value2, value3, value4, value5, value6, value7);
-        }
-
-        public static void Bind<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8)
-        {
-            action(value1, value2, value3, value4, value5, value6, value7, value8);
-        }
-
-        public static TResult Bind<T, TResult>(Func<T, TResult> func, T value)
-        {
-            return func(value);
-        }
-
-        public static TResult Bind<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 value1, T2 value2)
-        {
-            return func(value1, value2);
-        }
-
-        public static TResult Bind<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> func, T1 value1, T2 value2, T3 value3)
-        {
-            return func(value1, value2, value3);
-        }
-
-        public static TResult Bind<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> func, T1 value1, T2 value2, T3 value3, T4 value4)
-        {
-            return func(value1, value2, value3, value4);
-        }
-
-        public static TResult Bind<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> func, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5)
-        {
-            return func(value1, value2, value3, value4, value5);
-        }
-
-        public static TResult Bind<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult> func, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6)
-        {
-            return func(value1, value2, value3, value4, value5, value6);
-        }
-
-        public static TResult Bind<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, TResult> func, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7)
-        {
-            return func(value1, value2, value3, value4, value5, value6, value7);
-        }
-
-        public static TResult Bind<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> func, T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8)
-        {
-            return func(value1, value2, value3, value4, value5, value6, value7, value8);
-        }
-
         public static Func<T2, TResult> Bind1st<T1, T2, TResult>(this Func<T1, T2, TResult> func, T1 value1)
         {
             return new Func<T2, TResult>(value2 => func(value1, value2));
@@ -288,177 +208,177 @@ namespace XSpect
 
         public static Action<T2> Bind1st<T1, T2>(this Action<T1, T2> action, T1 value1)
         {
-            return new Func<T2>(value2 => action(value1, value2));
+            return new Action<T2>(value2 => action(value1, value2));
         }
 
         public static Action<T1> Bind2nd<T1, T2>(this Action<T1, T2> action, T2 value2)
         {
-            return new Func<T1>(value1 => action(value1, value2));
+            return new Action<T1>(value1 => action(value1, value2));
         }
 
         public static Action<T2, T3> Bind1st<T1, T2, T3>(this Action<T1, T2, T3> action, T1 value1)
         {
-            return new Func<T2, T3>((value2, value3) => action(value1, value2, value3));
+            return new Action<T2, T3>((value2, value3) => action(value1, value2, value3));
         }
 
         public static Action<T1, T3> Bind2nd<T1, T2, T3>(this Action<T1, T2, T3> action, T2 value2)
         {
-            return new Func<T1, T3>((value1, value3) => action(value1, value2, value3));
+            return new Action<T1, T3>((value1, value3) => action(value1, value2, value3));
         }
 
         public static Action<T1, T2> Bind3rd<T1, T2, T3>(this Action<T1, T2, T3> action, T3 value3)
         {
-            return new Func<T1, T2>((value1, value2) => action(value1, value2, value3));
+            return new Action<T1, T2>((value1, value2) => action(value1, value2, value3));
         }
 
         public static Action<T2, T3, T4> Bind1st<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T1 value1)
         {
-            return new Func<T2, T3, T4>((value2, value3, value4) => action(value1, value2, value3, value4));
+            return new Action<T2, T3, T4>((value2, value3, value4) => action(value1, value2, value3, value4));
         }
 
         public static Action<T1, T3, T4> Bind2nd<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T2 value2)
         {
-            return new Func<T1, T3, T4>((value1, value3, value4) => action(value1, value2, value3, value4));
+            return new Action<T1, T3, T4>((value1, value3, value4) => action(value1, value2, value3, value4));
         }
 
         public static Action<T1, T2, T4> Bind3rd<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T3 value3)
         {
-            return new Func<T1, T2, T4>((value1, value2, value4) => action(value1, value2, value3, value4));
+            return new Action<T1, T2, T4>((value1, value2, value4) => action(value1, value2, value3, value4));
         }
 
         public static Action<T1, T2, T3> Bind4th<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action, T4 value4)
         {
-            return new Func<T1, T2, T3>((value1, value2, value3) => action(value1, value2, value3, value4));
+            return new Action<T1, T2, T3>((value1, value2, value3) => action(value1, value2, value3, value4));
         }
 
         public static Action<T2, T3, T4, T5> Bind1st<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T1 value1)
         {
-            return new Func<T2, T3, T4, T5>((value2, value3, value4, value5) => action(value1, value2, value3, value4, value5));
+            return new Action<T2, T3, T4, T5>((value2, value3, value4, value5) => action(value1, value2, value3, value4, value5));
         }
 
         public static Action<T1, T3, T4, T5> Bind2nd<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T2 value2)
         {
-            return new Func<T1, T3, T4, T5>((value1, value3, value4, value5) => action(value1, value2, value3, value4, value5));
+            return new Action<T1, T3, T4, T5>((value1, value3, value4, value5) => action(value1, value2, value3, value4, value5));
         }
 
         public static Action<T1, T2, T4, T5> Bind3rd<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T3 value3)
         {
-            return new Func<T1, T2, T4, T5>((value1, value2, value4, value5) => action(value1, value2, value3, value4, value5));
+            return new Action<T1, T2, T4, T5>((value1, value2, value4, value5) => action(value1, value2, value3, value4, value5));
         }
 
         public static Action<T1, T2, T3, T5> Bind4th<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T4 value4)
         {
-            return new Func<T1, T2, T3, T5>((value1, value2, value3, value5) => action(value1, value2, value3, value4, value5));
+            return new Action<T1, T2, T3, T5>((value1, value2, value3, value5) => action(value1, value2, value3, value4, value5));
         }
 
         public static Action<T1, T2, T3, T4> Bind5th<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action, T5 value5)
         {
-            return new Func<T1, T2, T3, T4>((value1, value2, value3, value4) => action(value1, value2, value3, value4, value5));
+            return new Action<T1, T2, T3, T4>((value1, value2, value3, value4) => action(value1, value2, value3, value4, value5));
         }
 
         public static Action<T2, T3, T4, T5, T6> Bind1st<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T1 value1)
         {
-            return new Func<T2, T3, T4, T5, T6>((value2, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T2, T3, T4, T5, T6>((value2, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T1, T3, T4, T5, T6> Bind2nd<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T2 value2)
         {
-            return new Func<T1, T3, T4, T5, T6>((value1, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T1, T3, T4, T5, T6>((value1, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T1, T2, T4, T5, T6> Bind3rd<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T3 value3)
         {
-            return new Func<T1, T2, T4, T5, T6>((value1, value2, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T1, T2, T4, T5, T6>((value1, value2, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T1, T2, T3, T5, T6> Bind4th<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T4 value4)
         {
-            return new Func<T1, T2, T3, T5, T6>((value1, value2, value3, value5, value6) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T1, T2, T3, T5, T6>((value1, value2, value3, value5, value6) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T1, T2, T3, T4, T6> Bind5th<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T5 value5)
         {
-            return new Func<T1, T2, T3, T4, T6>((value1, value2, value3, value4, value6) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T1, T2, T3, T4, T6>((value1, value2, value3, value4, value6) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T1, T2, T3, T4, T5> Bind6th<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action, T6 value6)
         {
-            return new Func<T1, T2, T3, T4, T5>((value1, value2, value3, value4, value5) => action(value1, value2, value3, value4, value5, value6));
+            return new Action<T1, T2, T3, T4, T5>((value1, value2, value3, value4, value5) => action(value1, value2, value3, value4, value5, value6));
         }
 
         public static Action<T2, T3, T4, T5, T6, T7> Bind1st<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T1 value1)
         {
-            return new Func<T2, T3, T4, T5, T6, T7>((value2, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T2, T3, T4, T5, T6, T7>((value2, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T3, T4, T5, T6, T7> Bind2nd<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T2 value2)
         {
-            return new Func<T1, T3, T4, T5, T6, T7>((value1, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T3, T4, T5, T6, T7>((value1, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T2, T4, T5, T6, T7> Bind3rd<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T3 value3)
         {
-            return new Func<T1, T2, T4, T5, T6, T7>((value1, value2, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T2, T4, T5, T6, T7>((value1, value2, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T2, T3, T5, T6, T7> Bind4th<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T4 value4)
         {
-            return new Func<T1, T2, T3, T5, T6, T7>((value1, value2, value3, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T2, T3, T5, T6, T7>((value1, value2, value3, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T2, T3, T4, T6, T7> Bind5th<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T5 value5)
         {
-            return new Func<T1, T2, T3, T4, T6, T7>((value1, value2, value3, value4, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T2, T3, T4, T6, T7>((value1, value2, value3, value4, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T2, T3, T4, T5, T7> Bind6th<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T6 value6)
         {
-            return new Func<T1, T2, T3, T4, T5, T7>((value1, value2, value3, value4, value5, value7) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T2, T3, T4, T5, T7>((value1, value2, value3, value4, value5, value7) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T1, T2, T3, T4, T5, T6> Bind7th<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action, T7 value7)
         {
-            return new Func<T1, T2, T3, T4, T5, T6>((value1, value2, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6, value7));
+            return new Action<T1, T2, T3, T4, T5, T6>((value1, value2, value3, value4, value5, value6) => action(value1, value2, value3, value4, value5, value6, value7));
         }
 
         public static Action<T2, T3, T4, T5, T6, T7, T8> Bind1st<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T1 value1)
         {
-            return new Func<T2, T3, T4, T5, T6, T7, T8>((value2, value3, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T2, T3, T4, T5, T6, T7, T8>((value2, value3, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T3, T4, T5, T6, T7, T8> Bind2nd<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T2 value2)
         {
-            return new Func<T1, T3, T4, T5, T6, T7, T8>((value1, value3, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T3, T4, T5, T6, T7, T8>((value1, value3, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T4, T5, T6, T7, T8> Bind3rd<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T3 value3)
         {
-            return new Func<T1, T2, T4, T5, T6, T7, T8>((value1, value2, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T4, T5, T6, T7, T8>((value1, value2, value4, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T3, T5, T6, T7, T8> Bind4th<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T4 value4)
         {
-            return new Func<T1, T2, T3, T5, T6, T7, T8>((value1, value2, value3, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T3, T5, T6, T7, T8>((value1, value2, value3, value5, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T3, T4, T6, T7, T8> Bind5th<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T5 value5)
         {
-            return new Func<T1, T2, T3, T4, T6, T7, T8>((value1, value2, value3, value4, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T3, T4, T6, T7, T8>((value1, value2, value3, value4, value6, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T3, T4, T5, T7, T8> Bind6th<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T6 value6)
         {
-            return new Func<T1, T2, T3, T4, T5, T7, T8>((value1, value2, value3, value4, value5, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T3, T4, T5, T7, T8>((value1, value2, value3, value4, value5, value7, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T3, T4, T5, T6, T8> Bind7th<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T7 value7)
         {
-            return new Func<T1, T2, T3, T4, T5, T6, T8>((value1, value2, value3, value4, value5, value6, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T3, T4, T5, T6, T8>((value1, value2, value3, value4, value5, value6, value8) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
 
         public static Action<T1, T2, T3, T4, T5, T6, T7> Bind8th<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action, T8 value8)
         {
-            return new Func<T1, T2, T3, T4, T5, T6, T7>((value1, value2, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7, value8));
+            return new Action<T1, T2, T3, T4, T5, T6, T7>((value1, value2, value3, value4, value5, value6, value7) => action(value1, value2, value3, value4, value5, value6, value7, value8));
         }
     }
 }
