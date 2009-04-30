0dc5e1b627502ad150bc668ac1c90e67e6356f82
Takeshi KIRIYA <takeshik@users.sf.net>
2009-04-30 04:57:45 +0000

    Imported commit (revision 265) from Subversion repository
    Implement TwitterWebInput (not tested)

--- TwitterWebFlow/TwitterWebInput.cs
+++ TwitterWebFlow/TwitterWebInput.cs
@@ -125,47 +125,52 @@ namespace XSpect.MetaTweet
         [FlowInterface("/home")]
         public IEnumerable<StorageObject> FetchPublicTimeline(StorageModule storage, String param, IDictionary<String, String> args)
         {
-            throw new NotImplementedException();
+            DateTime now = DateTime.Now;
+            return this._client.Get(new Uri("https://twitter.com/home" + args.ToUriQuery()), this._processor).XPathSelectElements(
+                this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+                "xpath-e:statuses.status",
+                "//ol[@id='timeline']/li"
+            )).Select(xelement => this.AnalyzeStatus(xelement, now, storage)).Cast<StorageObject>();
         }
 
-        private IEnumerable<Post> FetchPosts(XDocument xdoc, DateTime createdAt, StorageModule storage)
+        private Post AnalyzeStatus(XElement xstatus, DateTime timestamp, StorageModule storage)
         {
             // Int32 id = ?
-            String name = xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            String name = xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.name",
                 "string(.//a[@class='screen-name']/@title)"
             ));
-            String screenName = xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            String screenName = xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.screenName",
                 "string(.//a[@class='screen-name'])"
             ));
             // String location = ?
             // String description = ?
-            Uri profileImageUri = new Uri(xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            Uri profileImageUri = new Uri(xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.imageUri",
                 "string(.//img[contains(@class,'photo')]/@src)"
             )));
             // String uri = ?
-            Boolean isProtected = xdoc.XPathEvaluate<Boolean>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            Boolean isProtected = xstatus.XPathEvaluate<Boolean>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-b:statuses.status.protected",
                 "boolean(//img[@class='lock'])"
             ));
             // Int32 followersCount = ?
 
-            Int32 statusId = Int32.Parse(xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            Int32 statusId = Int32.Parse(xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.id",
                 "substring-after(string(@id),'status_')"
             )));
-            String text = xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            String text = xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.body",
                 "string(.//span[@class='entry-content'])"
             ));
-            String source = xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            String source = xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.source",
                 "string(.//span[count(./@*)=0]/a)"
             ));
             // Boolean isTruncated = ?
-            String inReplyTo = xdoc.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            String inReplyTo = xstatus.XPathEvaluate<String>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-s:statuses.status.inReplyTo",
                 "string(.//a[starts-with(string(.), 'in')][contains(string(.), 'reply')]/@href)"
             ));
@@ -181,11 +186,111 @@ namespace XSpect.MetaTweet
                     "twitter.com/(.+)/status"
                   ).RegexMatch(inReplyTo).Groups[1].Value
                 : null;
-            Boolean isFavorited = xdoc.XPathEvaluate<Boolean>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
+            Boolean isFavorited = xstatus.XPathEvaluate<Boolean>(this.Configuration.GetChild("scrapingKeys").GetValueOrDefault(
                 "xpath-b:statuses.status.favorited",
                 "not(boolean(//a[contains(@class,'non-fav')]))"
             ));
-            throw new NotImplementedException();
+
+            Activity userActivity = storage
+                .GetActivities()
+                .Where(a => a.Category == "ScreenName" && a.Value == screenName)
+                .OrderByDescending(a => a.Timestamp)
+                .ThenBy(a => a.Subindex)
+                .SingleOrDefault();
+
+            Account account;
+            if (userActivity == null)
+            {
+                account = storage.NewAccount(Guid.NewGuid(), this.Realm);
+            }
+            else
+            {
+                account = userActivity.GetAccount();
+            }
+
+            Activity activity;
+
+            // TODO: test whether timestamp is status/created_at or DateTime.Now (responsed at).
+
+            // User analyzing:
+
+            // Id?
+
+            if ((activity = account.GetActivityOf("Name", timestamp)) == null
+                ? (activity = account.NewActivity(timestamp, "Name")) != null /* true */
+                : activity.Value != name
+            )
+            {
+                activity.Value = name;
+            }
+
+            if ((activity = account.GetActivityOf("ScreenName", timestamp)) == null
+                ? (activity = account.NewActivity(timestamp, "ScreenName")) != null /* true */
+                : activity.Value != screenName
+            )
+            {
+                activity.Value = screenName;
+            }
+
+            // Location?
+
+            // Description?
+
+            if ((activity = account.GetActivityOf("ProfileImage", timestamp)) == null
+                ? (activity = account.NewActivity(timestamp, "ProfileImage")) != null /* true */
+                : activity.Value != profileImageUri.ToString()
+            )
+            {
+                activity.Value = profileImageUri.ToString();
+                // TODO: Fetching ImageUri / Thumbnail (or original) activity?
+            }
+
+            // Uri?
+
+            if ((activity = account.GetActivityOf("IsRestricted", timestamp)) == null
+                ? (activity = account.NewActivity(timestamp, "IsRestricted")) != null /* true */
+                : activity.Value != isProtected.ToString()
+            )
+            {
+                activity.Value = isProtected.ToString();
+            }
+
+            // FollowersCount?
+
+            // Status analyzing:
+
+            if ((activity = storage.GetActivities(
+                r => r.AccountId == account.AccountId
+                  && r.Category == "Post"
+                  && r.Value == statusId.ToString()
+                ).SingleOrDefault()) == null
+            )
+            {
+                activity = account.NewActivity(timestamp, "Post");
+                activity.Value = statusId.ToString();
+            }
+
+            Post post = activity.ToPost();
+            post.Text = text;
+            post.Source = source;
+            if (inReplyToStatusId.HasValue)
+            {/*
+                // Load in-reply-to from the backend
+                storage.LoadPostsDataTable(null, inReplyToStatusId.Value.ToString());
+                
+                // debug.
+                var x = storage.GetPosts(r => r.PostId == inReplyToStatusId.Value.ToString());
+                if (x.Count > 1)
+                {
+                    System.Diagnostics.Debugger.Break();
+                }
+                Post inReplyToPost = x.SingleOrDefault();
+                if (inReplyToPost != null)
+                {
+                    post.AddReplying(inReplyToPost);
+                }*/
+            }
+            return post;
         }
     }
 }
