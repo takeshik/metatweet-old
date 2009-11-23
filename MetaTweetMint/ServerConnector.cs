430a6322c91e0bd69b393ece0805efc7f0df2862
Takeshi KIRIYA <takeshik@users.sf.net>
2009-11-23 17:39:54 +0000

    Imported commit (revision 434) from Subversion repository
    Add client data model classes.

---
+++ MetaTweetMint/ServerConnector.cs
@@ -0,0 +1,108 @@
+﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
+// $Id$
+/* MetaTweet
+ *   Hub system for micro-blog communication services
+ * MetaTweetMint
+ *   Extensible GUI client for MetaTweet
+ *   Part of MetaTweet
+ * Copyright © 2009 Takeshi KIRIYA, XSpect Project <takeshik@users.sf.net>
+ * All rights reserved.
+ * 
+ * This file is part of MetaTweetMint.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>,
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+
+namespace XSpect.MetaTweet.Clients.Mint
+{
+    public class ServerConnector
+        : Object
+    {
+        private IChannel _channel;
+
+        public String Name
+        {
+            get;
+            private set;
+        }
+
+        public Boolean IsConnected
+        {
+            get
+            {
+                return this.Host != null;
+            }
+        }
+
+        public IChannel Channel
+        {
+            get
+            {
+                return this._channel;
+            }
+            set
+            {
+                if (this.IsConnected)
+                {
+                    throw new InvalidOperationException("Channel is now used.");
+                }
+                this._channel = value;
+            }
+        }
+
+        public ServerCore Host
+        {
+            get;
+            private set;
+        }
+
+        public IList<ObjectView> Views
+        {
+            get;
+            private set;
+        }
+
+        public ServerConnector()
+        {
+            this.Views = new List<ObjectView>();
+        }
+
+        public void Connect(Uri uri)
+        {
+            if (!this.IsConnected)
+            {
+                ChannelServices.RegisterChannel(this._channel, true);
+                RemotingConfiguration.RegisterWellKnownClientType(typeof(ServerCore), uri.ToString());
+                this.Host = Activator.CreateInstance<ServerCore>();
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (this.IsConnected)
+            {
+                this.Host = null;
+                ChannelServices.UnregisterChannel(this._channel);
+            }
+        }
+    }
+}
