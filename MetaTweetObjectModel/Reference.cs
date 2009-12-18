1850e79b90135d033710c9009f8b00b83f061965
Takeshi KIRIYA <takeshik@users.sf.net>
2009-12-19 02:56:44 +0900

    Fix some files
    Remove mis-added BOM in re-created source files in MetaTweetObjectModel.
    Remove informations about XSpectCommonFramework in README texts.

--- MetaTweetObjectModel/Reference.cs
+++ MetaTweetObjectModel/Reference.cs
@@ -1,4 +1,4 @@
-﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 // vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
 // $Id$
 /* MetaTweet
