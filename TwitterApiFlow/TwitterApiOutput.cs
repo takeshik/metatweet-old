212f965269a4f807c11c37f66b5cfc22402722e2
Takeshi KIRIYA <takeshik@users.sf.net>
2010-02-21 10:46:17 +0900

    Support users output in TwitterApiOutput
    Support to output accounts as <users> XML output.
    Add MetaTweet AccountId GUID outputs in user element as a XML element.

--- TwitterApiFlow/TwitterApiOutput.cs
+++ TwitterApiFlow/TwitterApiOutput.cs
@@ -63,55 +63,72 @@ namespace XSpect.MetaTweet.Modules
         [FlowInterface("/.xml", WriteTo = StorageObjectTypes.None)]
         public String OutputTwitterXmlFormat(IEnumerable<StorageObject> source, StorageModule storage, String param, IDictionary<String, String> args)
         {
+            String type = source.All(o => o is Activity && (o as Activity).Category == "Post")
+                ? "statuses"
+                : source.All(o => o is Account)
+                      ? "users"
+                      : "objects";
             this.CheckMyself(storage);
             return new StringWriter().Let(
                 // TODO: Support <users> output: check source elements' type?
                 new XDocument(
                     new XDeclaration("1.0", "utf-16", "yes"),
-                    new XElement("statuses",
+                    new XElement(type,
                         new XAttribute("type", "array"),
                         new XAttribute("metatweet-version", ThisAssembly.EntireCommitId),
-                        source
-                            .OfType<Activity>()
-                            .Where(a => a.Category == "Post")
-                            .Select(a =>
-                                new XElement("status",
-                                    new XElement("created_at", a.Timestamp
-                                        .ToString("ddd MMM dd HH:mm:ss +0000 yyyy", CultureInfo.InvariantCulture)
-                                    ),
-                                    new XElement("id", a.SubId),
-                                    new XElement("text", a.Value),
-                                    new XElement("source", "<a href=\"zapped\"> rel=\"nofollow\">" + a.UserAgent + "</a>"),
-                                    new XElement("truncated", "false"),
-                                    a.ReferrersOf("Mention")
-                                    // TODO: First?
-                                        .FirstOrDefault()
-                                        .Do(m => Make.Array(
-                                            new XElement("in_reply_to_status_id", m.Null(_ => _.SubId)),
-                                            new XElement("in_reply_to_user_id", m.Null(_ => _.Account["Id"].Value)),
-                                            new XElement("in_reply_to_screen_name", m.Null(_ => _.Account["ScreenName"].Value))
-                                        )),
-                                    new XElement("favorited", a.IsMarked("Favorite", this.Myself).ToString().ToLower()),
-                                    new XElement("user",
-                                        new XElement("id", a.Account["Id"].Value),
-                                        new XElement("name", a.Account["Name"].Value),
-                                        new XElement("screen_name", a.Account["ScreenName"].Value),
-                                        new XElement("location", a.Account["Location"].Value),
-                                        new XElement("profile_image_url", a.Account["ProfileImage"].Value),
-                                        new XElement("url", a.Account["Uri"].Value),
-                                        new XElement("followers_count", a.Account["FollowersCount"].Value),
-                                        new XElement("friends_count", a.Account["FollowingCount"].Value),
-                                        new XElement("created_at", DateTime.Parse(a.Account["CreatedAt"].Value)
-                                            .ToString("ddd MMM dd HH:mm:ss +0000 yyyy", CultureInfo.InvariantCulture)
-                                        ),
-                                        new XElement("favourites_count", a.Account["FavoritesCount"].Value),
-                                        new XElement("statuses_count", a.Account["StatusesCount"].Value),
-                                        new XElement("following", a.Account.IsRelated("Follow", this.Myself))
-                                    )
-                                )
-                            )
+                        source.Select(o => o is Account
+                            ? this.OutputUser(o as Account, true)
+                            : o is Activity && (o as Activity).Category == "Post"
+                                  ? this.OutputStatus(o as Activity, true)
+                                  : new XElement("not-supported-object")
+                        )
                     )
                 ).Save).ToString();
+        }
+
+        private XElement OutputStatus(Activity activity, Boolean includesUser)
+        {
+            return new XElement("status",
+                new XElement("created_at", activity.Timestamp
+                    .ToString("ddd MMM dd HH:mm:ss +0000 yyyy", CultureInfo.InvariantCulture)
+                ),
+                new XElement("id", activity.SubId),
+                new XElement("text", activity.Value),
+                new XElement("source", "<a href=\"zapped\"> rel=\"nofollow\">" + activity.UserAgent + "</a>"),
+                new XElement("truncated", "false"),
+                activity.ReferrersOf("Mention")
+                // TODO: First?
+                    .FirstOrDefault()
+                    .Do(m => Make.Array(
+                        new XElement("in_reply_to_status_id", m.Null(_ => _.SubId)),
+                        new XElement("in_reply_to_user_id", m.Null(_ => _.Account["Id"].Value)),
+                        new XElement("in_reply_to_screen_name", m.Null(_ => _.Account["ScreenName"].Value))
+                    )),
+                new XElement("favorited", activity.IsMarked("Favorite", this.Myself).ToString().ToLower()),
+                includesUser ? Make.Array(this.OutputUser(activity.Account, false)) : null
+            );
+        }
+
+        private XElement OutputUser(Account account, Boolean includesStatus)
+        {
+            return new XElement("user",
+                new XAttribute("metatweet-account-id", account.AccountId),
+                new XElement("id", account["Id"].Value),
+                new XElement("name", account["Name"].Value),
+                new XElement("screen_name", account["ScreenName"].Value),
+                new XElement("location", account["Location"].Value),
+                new XElement("profile_image_url", account["ProfileImage"].Value),
+                new XElement("url", account["Uri"].Value),
+                new XElement("followers_count", account["FollowersCount"].Value),
+                new XElement("friends_count", account["FollowingCount"].Value),
+                new XElement("created_at", DateTime.Parse(account["CreatedAt"].Value)
+                    .ToString("ddd MMM dd HH:mm:ss +0000 yyyy", CultureInfo.InvariantCulture)
+                ),
+                new XElement("favourites_count", account["FavoritesCount"].Value),
+                new XElement("statuses_count", account["StatusesCount"].Value),
+                new XElement("following", account.IsRelated("Follow", this.Myself)),
+                includesStatus && account["Post"] != null ? Make.Array(this.OutputStatus(account["Post"], false)) : null
+            );
         }
 
         private void CheckMyself(StorageModule storage)
