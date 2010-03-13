e5836a072dc76f71497600fd488ae4b84a96b6c2
Takeshi KIRIYA <takeshik@users.sf.net>
2010-03-14 01:21:50 +0900

    Modify install / uninstall script
    Modify install-service.bat and uninstall-service.bat for x86 systems, force to
    use 32-bit InstallUtil.exe.

--- util/x86/DebugNoSign/install-service.bat
+++ util/x86/DebugNoSign/install-service.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %~DP0
 
-..\..\..\lib\ipy.exe ..\..\InstallUtil.py Install DebugNoSign x86 ..\..\..\dist\
+..\..\..\lib\ipy.exe ..\..\InstallUtil.py Install DebugNoSign x86 ..\..\..\dist\ 32
