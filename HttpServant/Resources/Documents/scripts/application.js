0f643b503654c3840b26b8ca3d605ebc4d0f534f
Takeshi KIRIYA <takeshik@users.sf.net>
2010-05-27 18:45:58 +0900

    Modify stylesheet and script for webpage, preparation for table output
    Modify application.css, add styles for table elements.
    Modify application.js, add functions to markup /.table.json response.

--- HttpServant/Resources/Documents/scripts/application.js
+++ HttpServant/Resources/Documents/scripts/application.js
@@ -1,6 +1,23 @@
 ﻿/// <reference path="jquery.js" />
 /// <reference path="jquery.linq.js" />
 
-$(document).ready(function() {
-    $("h1").text()
-});
+function markup(obj, idx) {
+    return "<tr class='" + (idx % 2 == 0 ? "even" : "odd") + "'>" +
+        $.Enumerable.From(obj)
+            .ToString("", function(c) {
+                return "<td>" + c + "</td>";
+            });
+}
+
+function toTable(obj, selector) {
+    var seq = $.Enumerable.From(obj);
+    $(selector, $(document)).html(
+        "<table><thead>" +
+        markup(seq.First()) +
+        "</thead><tbody>" +
+        seq.Skip(1).Select(function(r, i) {
+            return markup(r, i);
+        })
+        .ToString()
+    );
+}
