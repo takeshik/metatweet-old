bffa254572972fd306a40d20c517c5c2eb021eeb
Takeshi KIRIYA <takeshik@users.sf.net>
2010-05-28 06:01:07 +0900

    Add query page, add and modify scripts and styles
    Add query.haml page template and its script file.
    Implement server requesting via web.

--- HttpServant/Resources/Documents/scripts/application.js
+++ HttpServant/Resources/Documents/scripts/application.js
@@ -1,23 +1,3 @@
 ﻿/// <reference path="jquery.js" />
 /// <reference path="jquery.linq.js" />
 
-function markup(obj, idx) {
-    return "<tr class='" + (idx % 2 == 0 ? "even" : "odd") + "'>" +
-        $.Enumerable.From(obj)
-            .ToString("", function(c) {
-                return "<td>" + c + "</td>";
-            });
-}
-
-function toTable(obj, selector) {
-    var seq = $.Enumerable.From(obj);
-    $(selector, $(document)).html(
-        "<table><thead>" +
-        markup(seq.First()) +
-        "</thead><tbody>" +
-        seq.Skip(1).Select(function(r, i) {
-            return markup(r, i);
-        })
-        .ToString()
-    );
-}
---
+++ HttpServant/Resources/Documents/scripts/query.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="jquery.js" />
+/// <reference path="jquery.linq.js" />
+
+function markup(obj, idx) {
+    return "<tr class='" + (idx % 2 == 0 ? "even" : "odd") + "'>" +
+        $.Enumerable.From(obj)
+            .ToString("", function(c) {
+                return "<td>" + c + "</td>";
+            });
+}
+
+function request(requestString) {
+    $.ajax({
+        url: requestString,
+        dataType: 'json',
+        cache: false,
+        beforeSend: function(req) {
+            $("#resultHeader").text("Waiting for response.");
+            return true;
+        },
+        success: function(ret) {
+            var seq = $.Enumerable.From(ret);
+            $("#result").html(
+                "<table><thead>" +
+                markup(seq.First()) +
+                "</thead><tbody>" +
+                seq.Skip(1).Select(function(r, i) {
+                    return markup(r, i);
+                })
+                .ToString()
+            );
+            $("#resultHeader").text("Result");
+            },
+        error: function(req, textStatus, errorThrown) {
+            $("#resultHeader").html("ERROR: " + textStatuss);
+        }
+    });
+}
+
+function requestString_keyPress(event) {
+    if ((event ? event : window.event).keyCode == 13) {
+        $("#requestButton").click();
+        return false;
+    } else {
+        return true;
+    }
+}
