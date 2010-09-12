e0f31d2941b54ed83d9c3a5564afd92039f55154
Takeshi KIRIYA <takeshik@users.sf.net>
2010-09-13 03:44:35 +0900

    Add SstpServant project
    Add SstpServant project, a servant module to provide SSTP client feature.
    Add and modify default configuration files.

---
+++ SstpServant/Properties/AssemblyInfo.cs
@@ -0,0 +1,71 @@
+﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
+// $Id$
+/* MetaTweet
+ *   Hub system for micro-blog communication services
+ * SstpServant
+ *   MetaTweet Servant which provides SSTP (Sakura Script Transfer Protocol) client feature
+ *   Part of MetaTweet
+ * Copyright © 2008-2010 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
+ * All rights reserved.
+ * 
+ * This file is part of SQLiteStorage.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>,
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SstpServant")]
+[assembly: AssemblyDescription("MetaTweet SQLite Storage module")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XSpect Project")]
+[assembly: AssemblyProduct("MetaTweet")]
+[assembly: AssemblyCopyright("Copyright © 2008-2010 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>, All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("563b7b0f-5f8b-45e0-8611-ef357d5897a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(ThisAssembly.FileVersion)]
+
+#pragma warning disable 1699
+#if !NOSIGN
+[assembly: AssemblyKeyFile(@"../MetaTweet.snk")]
+[assembly: AssemblyDelaySign(false)]
+#endif
+#pragma warning restore 1699
---
+++ SstpServant/SstpServant.cs
@@ -0,0 +1,116 @@
+﻿// -*- mode: csharp; encoding: utf-8; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+// vim:set ft=cs fenc=utf-8 ts=4 sw=4 sts=4 et:
+// $Id$
+/* MetaTweet
+ *   Hub system for micro-blog communication services
+ * SstpServant
+ *   MetaTweet Servant which provides SSTP (Sakura Script Transfer Protocol) client feature
+ *   Part of MetaTweet
+ * Copyright © 2008-2010 Takeshi KIRIYA (aka takeshik) <takeshik@users.sf.net>
+ * All rights reserved.
+ * 
+ * This file is part of SQLiteStorage.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>,
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using XSpect.MetaTweet.Objects;
+using XSpect.Extension;
+
+namespace XSpect.MetaTweet.Modules
+{
+    public class SstpServant
+        : ServantModule
+    {
+        private readonly Thread _thread;
+
+        public String ServerAddress
+        {
+            get;
+            private set;
+        }
+
+        public Int32 ServerPort
+        {
+            get;
+            private set;
+        }
+
+        public String StorageName
+        {
+            get;
+            private set;
+        }
+
+        public SstpServant()
+        {
+            this._thread = new Thread(this.Notify);
+        }
+
+        protected override void ConfigureImpl()
+        {
+            this.ServerAddress = this.Configuration.ResolveValue<String>("serverAddress");
+            this.ServerPort = this.Configuration.ResolveValue<Int32>("serverPort");
+            this.StorageName = this.Configuration.ResolveValue<String>("storageName");
+            base.ConfigureImpl();
+        }
+
+        protected override void StartImpl()
+        {
+            this._thread.Start();
+        }
+
+        protected override void StopImpl()
+        {
+            this._thread.Abort();
+        }
+
+        private void Notify()
+        {
+            this.Host.ModuleManager.GetModule<StorageModule>(this.StorageName)
+                .ObjectCreated
+                .OfType<Activity>()
+                .Where(a => a.Category == "Post")
+                .Where(a => a.Value.EndsWith(@"\e"))
+                .Subscribe(this.Send);
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private void Send(Activity post)
+        {
+            using (TcpClient client = new TcpClient(this.ServerAddress, this.ServerPort))
+            {
+                client.GetStream()
+                    .Write(Encoding.UTF8.GetBytes(String.Format(
+                        // NOTE: Below here document expects newline code of this source is CRLF.
+                        #region SSTP
+@"SEND SSTP/1.4
+Sender: MetaTweet (from: {0})
+Script: {1}
+Charset: UTF-8
+"
+                        #endregion
+                        , post.Account["ScreenName"].Value, post.Value
+                    )));
+            }
+        }
+    }
+}
